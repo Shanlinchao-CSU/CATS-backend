c0200057818f9d7d8d75216229c82e6ee7f49afd
Harmony_Quan <3102676090@qq.com>
2024-02-18 11:27:49 +0800

    初始化

---
+++ src/main/java/com/example/cntsbackend/CntsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.cntsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CntsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CntsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cntsbackend/CntsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cntsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CntsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
