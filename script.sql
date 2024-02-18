7f31e0e8bedc0c43ec2ec57a82af724a6d90e8da
aaaxcb <2674314843@qq.com>
2024-02-18 15:32:22 +0800

    sql文件

---
+++ script.sql
@@ -0,0 +1,37 @@
+create table account
+(
+    account_id      int auto_increment
+        primary key,
+    account_name    varchar(255) null,
+    password        varchar(255) null,
+    phone           varchar(255) null,
+    email           varchar(255) null,
+    type            int          null,
+    enterprise_type int          null
+);
+
+create table register_application
+(
+    register_application_id int auto_increment
+        primary key,
+    account_name            varchar(255) null,
+    password                varchar(255) null,
+    phone                   varchar(255) null,
+    email                   varchar(255) null,
+    type                    int          null,
+    file_address            varchar(255) null
+);
+
+create table transaction
+(
+    transaction_id int auto_increment
+        primary key,
+    `from`         varchar(255)   null,
+    `to`           varchar(255)   null,
+    amount         decimal(10, 2) null,
+    start_time     datetime       null,
+    complete_time  datetime       null,
+    state          tinyint(1)     null
+);
+
+
