10834090bc3cbdcde8a10a37272b14fa1bd73f70
slc <1084884210@qq.com>
2024-03-08 14:39:41 +0800

    sql更新: 1.删除了所有的enterprise_address，不需要企业地址 2.在所有的需要审核的请求表中加了conductor_id，表示处理人id 3.添加了quota_sale表，表示在售额度 4.按照quota_sale表简化了transaction表 5.需要提醒下大家，卖额度的用户出售用户，购买者只需单方面购买即可完成交易，而无需卖方再次确定，因此transaction只有一个complete_time

--- script.sql
+++ script.sql
@@ -1,23 +1,49 @@
 create table account
 (
-    account_id         int auto_increment
+    account_id      int auto_increment
         primary key,
-    account_name       varchar(255) null,
-    password           varchar(255) null,
-    phone              varchar(255) null,
-    email              varchar(255) null,
-    type               int          null,
-    enterprise_type    int          null,
-    enterprise_address varchar(255) null,
-    file               varchar(255) null
-);
+    account_name    varchar(255) null,
+    password        varchar(255) null,
+    phone           varchar(255) null,
+    email           varchar(255) null,
+    type            int          null,
+    enterprise_type int          null,
+    file            varchar(255) null
+)
+    comment '用户表';
+
+create table accounting_record
+(
+    id                  int auto_increment comment '记录的id'
+        primary key,
+    enterprise_name     int          null comment '对应企业的name',
+    month               int          null comment '该碳核算记录对应的月份',
+    time                timestamp    null comment '生成该记录的具体时间',
+    state               int          null comment '0表示通过审核，1表示等待审核，2表示被拒绝',
+    variable_json       varchar(800) null comment '用来描述计算过程所需参数的json字符串',
+    result              varchar(100) null comment '碳核算结果',
+    supporting_material varchar(100) null comment '存文件保存路径',
+    conductor_id        int          null comment '处理这条请求的数据审核员的id'
+)
+    comment '碳核算请求';
 
 create table cmessage
 (
-    account_id int not null
+    account_id int    not null
         primary key,
-    climit     int null
+    t_remain   double null,
+    t_coin     double null
 );
+
+create table quota_sale
+(
+    id         int auto_increment
+        primary key,
+    quota      double null comment '出售的额度',
+    seller_id  int    null comment '售者id',
+    unit_price double null comment '单价'
+)
+    comment '在售额度';
 
 create table register_application
 (
@@ -30,45 +56,34 @@ create table register_application
     type                    int          null,
     file_address            varchar(255) null,
     enterprise_type         int          null,
-    enterprise_address      varchar(255) null
-);
+    conductor_id            int          null comment '处理这条申请的管理员的id'
+)
+    comment '注册申请表';
 
 create table transaction
 (
     transaction_id int auto_increment
         primary key,
-    t_from         varchar(255) null,
-    t_to           varchar(255) null,
-    amount         int          null,
-    start_time     datetime     null,
-    complete_time  datetime     null,
-    state          tinyint(1)   null
-);
+    amount         double    null,
+    buyer_id       int       null,
+    sale_id        int       null comment '对应quota_sale的id，这里就不设外键了',
+    cost           double    null,
+    complete_time  timestamp null
+)
+    comment '交易表';
 
 create table update_account
 (
-    account_id         int auto_increment
+    account_id      int auto_increment
         primary key,
-    account_name       varchar(255) null,
-    password           varchar(255) null,
-    phone              varchar(255) null,
-    email              varchar(255) null,
-    type               int          null,
-    enterprise_type    int          null,
-    enterprise_address varchar(255) null,
-    file               varchar(255) null
-);
-
-create table accounting_record
-(
-    id                  int auto_increment comment '记录的id'
-        primary key,
-    enterprise_id       int          null comment '对应企业的id',
-    month               int          null comment '该碳核算记录对应的月份',
-    time                timestamp    null comment '生成该记录的具体时间',
-    state               int          null comment '0表示通过审核，1表示等待审核，2表示被拒绝',
-    variable_json       varchar(800) null comment '用来描述计算过程所需参数的json字符串',
-    result              int          null comment '碳核算结果',
-    supporting_material varchar(100) null comment '存文件保存路径'
+    account_name    varchar(255) null,
+    password        varchar(255) null,
+    phone           varchar(255) null,
+    email           varchar(255) null,
+    type            int          null,
+    enterprise_type int          null,
+    file            varchar(255) null,
+    conductor_id    int          null comment '处理这条申请的管理员的id'
 )
-    comment '碳核算记录';
+    comment '修改信息申请表';
+
